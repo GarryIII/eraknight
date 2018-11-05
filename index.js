a2f926d996f14842d1db1c4bb1644e2b63e80784
GarryIII <44745581+GarryIII@users.noreply.github.com>
2018-11-05 12:35:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = process.env.token // a garder en version heroku
 const prefix = ("?");
-var prefix = (":");
 
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
