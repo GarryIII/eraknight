286e42022ff7787dd3b1262ea1e44227d161d980
GarryIII <44745581+GarryIII@users.noreply.github.com>
2018-11-05 10:40:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ bot.on('message', msg => {
         msg.reply("Heureux de te revoir parmis nous.")
     }
     if (msg.content.match(/salut/i)) {
-            msg.reply("Salutation.")
+            msg.reply('Salutaion.')
     }
     if (msg.content == "IP"){
             msg.reply('EraKnight V2 Soon.....')
