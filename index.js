e82cabdd3ae93c8320be074e7d57421a76545b75
GarryIII <44745581+GarryIII@users.noreply.github.com>
2018-11-05 10:33:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ bot.on('message', msg => {
         msg.reply("Heureux de te revoir parmis nous.")
     }
     if (msg.content.match(/salut/i)) {
-            msg.reply('Je suis d\'accord avec toi.')
+            msg.reply('Salutation.')
     }
     if (msg.content === prefix + "site"){
         msg.channel.send("En cours....")
