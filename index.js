34f1655f7cba14cee3f06427910c1024cde308cc
GarryIII <44745581+GarryIII@users.noreply.github.com>
2018-11-05 09:30:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ bot.on('ready', function () {
 
 bot.on('guildMemberAdd', member => {
     member.createDM().then(channel => {
-        return channel.send('Bienvenue sur le serveur EraKnight' + member.displayName)
+        return channel.send('Bienvenue sur le serveur' + member.displayName)
         console.log(`${member.displayName} à rejoind le serveur.`)
     }).catch(console.error)
 });
