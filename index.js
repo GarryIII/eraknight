ff772208dab3c6fd44f532d262c621f609fbdf7f
GarryIII <44745581+GarryIII@users.noreply.github.com>
2018-11-05 10:53:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ bot.on('message', function (message){
 
 bot.on('message', msg => {
     if (msg.content === "bonjour"){
-            msg.reply("Heureux de te revoir parmis nous.")
+        msg.reply("Heureux de te revoir parmis nous.")
     }
     if (msg.content.match(/salut/i)) {
             msg.reply('Je suis d\'accord avec toi.')
@@ -36,8 +36,8 @@ bot.on('message', msg => {
             msg.reply('EraKnight V2 Soon.....')
     }
     if (msg.content === prefix + "site"){
-            msg.channel.send("En cours....")
-                console.log("Une personne a demandé pour aller sur ton site.")
+        msg.channel.send("En cours....")
+        console.log("Une personne a demandé pour aller sur ton site.")
     }
 client.on('message', msg => {
     if (msg.content === 'ping') {
