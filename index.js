8d0f1b8b213d1c42cbd2d4097ab1becf61234d57
GarryIII <44745581+GarryIII@users.noreply.github.com>
2018-11-05 16:26:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ bot.on('message', msg => {
         msg.channel.send("En cours.... de développement")
         console.log("Une personne a demandé pour aller sur ton site.")
     }
-    if(cmd === `${prefix}info`){
+    if(msg.content === `${prefix}info`){
          
         let sicon = message.guild.iconURL;
         let serverembed = new Discord.RichEmbed()
