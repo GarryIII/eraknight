c0b9aeb5b91864a577ee24b4c80aa631ca1b5074
GarryIII <44745581+GarryIII@users.noreply.github.com>
2018-11-05 22:44:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ bot.on('message', msg => {
         var info_embed = new Discord.RichEmbed()
         .setColor("#40A497")
         .setTitle("Information du Discord!")
-        .addField("Nom du Discord", msg.guild.name.size)
+        .addField("Nom du Discord", msg.guild.name)
         .addField("IP du serveur", "EraKnight Soon....")
         .addField("Nombre de membres", msg.guild.members.size)
         .addField("Nombre de Salons", msg.guild.channels.size)
