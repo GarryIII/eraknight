ed5ef90c242a587ef7b1c63986aa4569495baa32
GarryIII <44745581+GarryIII@users.noreply.github.com>
2018-11-08 13:02:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ bot.on('message', msg => {
         msg.channel.send("En cours.... de développement")
         console.log("Une personne a demandé pour aller sur ton site.")
     }
-    if (msg.content === "Info + info") {
+    if (msg.content === "info") {
         var info_embed = new Discord.RichEmbed()
         .setColor("#40A497")
         .setTitle("Information du Discord!")
